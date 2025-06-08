953c3e7b7e0423dba9b4039ee10a3bc6f7063de8
Kirsty <congokirsty@gmail.com>
2025-06-08 20:01:04 +0100

    fixes contrast errors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
 <div className='w-full bg-[var(--bg-primary)] border-t border-[var(--border-accent)] p-2 text-xs '>
-  <p className="text-[var(--text-accent)] text-center font-light">
+  <p className="text-[var(--text-primary)] text-center">
     Data sources: The Metropolitan Museum of Art, Art Institute of Chicago, Smithsonian Institution. Usage complies with their open data policies.
     {' '}
     <Link to="/attribution" className="underline hover:text-blue-600">
       Learn more
     </Link>
   </p>
-  <p className='text-[var(--text-accent)] text-center font-light'><a target="_blank" href="https://icons8.com/icon/NN9Zc8VGiez5/museum">Museum</a> icon by <a target="_blank" href="https://icons8.com" >Icons8</a></p>
+  <p className='text-[var(--text-primary)] text-center'><a target="_blank" href="https://icons8.com/icon/NN9Zc8VGiez5/museum">Museum</a> icon by <a target="_blank" href="https://icons8.com" >Icons8</a></p>
 </div>
   )
 }
