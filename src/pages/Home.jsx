8ec801da0a9c5090b8f746803f02e986f1d82a74
Kirsty <congokirsty@gmail.com>
2025-06-09 10:53:49 +0100

    button labels added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
             filter: 'brightness(0.8)',
           }} />
 
-          <div className='relative z-10 text-center px-4 sm:mt-8'>
+          <div className='relative z-10 text-center px-4 pt-10'>
           {/* Heading */}
           <motion.h1
           initial={{ y:20, opacity:0 }}
@@ -54,9 +54,10 @@ const Home = () => {
           initial={{ y:20, opacity: 0}}
           animate={{ y:0, opacity: 1}}
           transition={{ delay: 0.6 }}
-          className='flex flex-col md:flex-row justify-center items-center gap-4 md:gap-8 place-self-center md:w-full sm:pb-10'
+          className='flex flex-col md:flex-row justify-center items-center gap-4 md:gap-8 place-self-center md:w-full sm:pb-16'
           >
             <Link
+              aria-label='Explore the artworks'
               to='/showcase'
               className='inline-flex items-center flex-shrink-0 space-x-2 bg-[var(--accent-primary)] text-[var(--button-text)] px-8 py-4 rounded-full text-lg font-semibold hover:bg-[var(--accent-secondary)] transition-colors'
             >
@@ -65,6 +66,7 @@ const Home = () => {
             </Link>
 
             <button
+              aria-label='Sign Up'
               onClick={() => setShowAuthModal(true)}
               className="inline-flex items-center justify-center w-40 flex-shrink-0 font-semibold text-[var(--button-text)] hover:text-[var(--accent-hover)] transition-colors  py-4 rounded-full cursor-pointer border-2 border-[var(--border-primary)]"
             >
