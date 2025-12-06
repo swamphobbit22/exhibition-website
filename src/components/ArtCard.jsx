55844ecb587131fd7e2adb894d8160e810233777
Kirsty <congokirsty@gmail.com>
2025-12-06 22:45:02 +0000

    update api

--- src/components/ArtCard.jsx
+++ src/components/ArtCard.jsx
@@ -56,6 +56,8 @@ const ArtCard = ({ artwork, detailUrl }) => {
               <motion.img 
                 src={imageUrl} 
                 alt={artwork.title || 'untitled'} 
+                crossOrigin="anonymous"
+                referrerPolicy="no-referrer-when-downgrade"
                 initial={{ y:20, opacity: 0 }}
                 animate={{ y:0,  opacity: 1 }}
                 transition={{ duratation: 0.3 }}
