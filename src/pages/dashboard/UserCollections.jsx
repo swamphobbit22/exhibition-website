33d19cfb29b90577891b9638eb03e0de112f794a
swamphobbit22 <117361295+swamphobbit22@users.noreply.github.com>
2025-12-31 10:56:27 +0000

    add dash button

--- src/pages/dashboard/UserCollections.jsx
+++ src/pages/dashboard/UserCollections.jsx
@@ -1,9 +1,18 @@
+import { useNaviagte } from 'react-router-dom';
 import UserCollectionsContent from "../../components/UserCollectionsContent";
 
 const UserCollections = () => {
 
+  const navigate = useNaviagte();
+
   return (
     <section className='min-h-screen bg-[var(--bg-primary)] pb-10 pt-28'>
+                <button 
+                  onClick={() => navigate('/dashboard')}
+                  className="mb-4 px-4 py-2 bg-[var(--accent-primary)] text-[var(--button-text)] hover:bg-[var(--accent-secondary)] rounded-full ml-10 cursor-pointer"
+                >
+                  Dashboard
+                </button>
         <UserCollectionsContent />
       </section>
   )
