c65098fe81fca9563e387ebec8d2f5222d95b800
Kirsty <congokirsty@gmail.com>
2025-12-31 11:59:58 +0000

    code tidy

--- src/pages/dashboard/UserCollections.jsx
+++ src/pages/dashboard/UserCollections.jsx
@@ -8,12 +8,12 @@ const UserCollections = () => {
   return (
     <section className='min-h-screen bg-[var(--bg-primary)] pb-10 pt-28'>
       <div className='justify-start'>
-                <button 
-                  onClick={() => navigate('/dashboard/home')}
-                  className="mb-4 px-4 py-2 bg-[var(--accent-primary)] text-[var(--button-text)] hover:bg-[var(--accent-secondary)] rounded-full ml-10 cursor-pointer font-bold"
-                >
-                  Dashboard
-                </button>
+        <button 
+          onClick={() => navigate('/dashboard/home')}
+          className="mb-4 px-4 py-2 bg-[var(--accent-primary)] text-[var(--button-text)] hover:bg-[var(--accent-secondary)] rounded-full ml-10 cursor-pointer font-bold"
+        >
+          Dashboard
+        </button>
       </div>
 
         <UserCollectionsContent />
