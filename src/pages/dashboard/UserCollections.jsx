e30f9522fe6febabc3022d330855bb7cf8613a71
Kirsty <congokirsty@gmail.com>
2025-12-31 11:44:54 +0000

    add dashboard button

--- src/pages/dashboard/UserCollections.jsx
+++ src/pages/dashboard/UserCollections.jsx
@@ -1,18 +1,21 @@
-import { useNaviagte } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import UserCollectionsContent from "../../components/UserCollectionsContent";
 
 const UserCollections = () => {
 
-  const navigate = useNaviagte();
+  const navigate = useNavigate();
 
   return (
     <section className='min-h-screen bg-[var(--bg-primary)] pb-10 pt-28'>
+      <div className='justify-start'>
                 <button 
                   onClick={() => navigate('/dashboard')}
-                  className="mb-4 px-4 py-2 bg-[var(--accent-primary)] text-[var(--button-text)] hover:bg-[var(--accent-secondary)] rounded-full ml-10 cursor-pointer"
+                  className="mb-4 px-4 py-2 bg-[var(--accent-primary)] text-[var(--button-text)] hover:bg-[var(--accent-secondary)] rounded-full ml-10 cursor-pointer font-bold"
                 >
                   Dashboard
                 </button>
+      </div>
+
         <UserCollectionsContent />
       </section>
   )
