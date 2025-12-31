6acf83dfaa216f6df8019263575ee7fd12b118d2
Kirsty <congokirsty@gmail.com>
2025-12-31 11:58:46 +0000

    fix dash button

--- src/pages/dashboard/UserCollections.jsx
+++ src/pages/dashboard/UserCollections.jsx
@@ -9,7 +9,7 @@ const UserCollections = () => {
     <section className='min-h-screen bg-[var(--bg-primary)] pb-10 pt-28'>
       <div className='justify-start'>
                 <button 
-                  onClick={() => navigate('/dashboard')}
+                  onClick={() => navigate('/dashboard/home')}
                   className="mb-4 px-4 py-2 bg-[var(--accent-primary)] text-[var(--button-text)] hover:bg-[var(--accent-secondary)] rounded-full ml-10 cursor-pointer font-bold"
                 >
                   Dashboard
